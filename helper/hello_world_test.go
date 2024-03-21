ce2bc510eb101a81c24a887e193331f5657c5cea
Annelies11 <mmahmudi1103@gmail.com>
2024-03-21 17:19:51 +0700

    assert

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,39 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSayHelloRequire(t *testing.T) {
+	result := SayHello("Aris")
+	require.Equal(t, "Hello Aris", result, "Harusnya 'Hello Aris' blok")
+	fmt.Println("Lha iki gak ketoro")
+}
+
+func TestSayHelloAssert(t *testing.T) {
+	result := SayHello("Aris")
+	assert.Equal(t, "Hello Aris", result, "Harusnya 'Hello Aris' blok")
+	fmt.Println("Test with Assert done")
+}
 
 func TestSayHello(t *testing.T) {
 	res := SayHello("Aris")
 
 	if res != "Hello Aris" {
-		panic("Hoi salah oi")
+		t.Fatal("Harusnya Hai Aris")
 	}
+	fmt.Println("TestSayHello Done")
 }
 
 func TestSayHelloBro(t *testing.T) {
 	res := SayHello("Bro")
 
 	if res != "Hello Bro" {
-		panic("Hoi salah oi")
+		t.FailNow()
 	}
+	fmt.Println("TestSayHelloBro Done")
 }
