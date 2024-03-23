c666d659869398ccf891ff7fb28f5e88ea0e556f
Annelies11 <mmahmudi1103@gmail.com>
2024-03-23 22:46:42 +0700

    before after test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMain(m *testing.M) {
+	fmt.Println("Before Testing")
+	m.Run()
+	fmt.Println("After Testing")
+}
+
 func TestSkip(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		t.Skip("Cannot run in Windows")
